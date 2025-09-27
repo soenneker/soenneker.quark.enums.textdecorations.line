5c12689031ef4d9758ec5606485d9478e8ece047
Jake Soenneker <jake@soeneker.com>
2025-09-27 09:14:15 -0500

    Renaming

---
+++ src/TextDecorationLineKeyword.cs
@@ -0,0 +1,24 @@
+using Intellenum;
+
+namespace Soenneker.Quark.Enums;
+
+/// <summary>
+/// CSS text decoration keywords in enumeration form.
+/// </summary>
+[Intellenum<string>]
+public partial class TextDecorationLineKeywordKeyword
+{
+    /// <summary>No text decoration.</summary>
+    public static readonly TextDecorationLineKeyword None = new("none");
+
+    /// <summary>Draws a line underneath the text.</summary>
+    public static readonly TextDecorationLineKeyword Underline = new("underline");
+
+    /// <summary>Draws a line above the text.</summary>
+    public static readonly TextDecorationLineKeyword Overline = new("overline");
+
+    /// <summary>Draws a line through the text.</summary>
+    public static readonly TextDecorationLineKeyword LineThrough = new("line-through");
+
+    public static implicit operator TextDecorationLineKeyword(GlobalKeyword style) => new(style.Value); // Value is the underlying string
+}
